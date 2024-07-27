a0a9298592b2e2965a483264d93c8d9142e069d5
bitof-KARTIK <157111737+bitof-KARTIK@users.noreply.github.com>
2024-07-27 17:32:58 +0530

    first commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,56 @@
+import type { Struct, Schema } from '@strapi/strapi';
+
+export interface ExperienceExperience extends Struct.ComponentSchema {
+  collectionName: 'components_experience_experiences';
+  info: {
+    displayName: 'Experience';
+    icon: 'arrowLeft';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    companyName: Schema.Attribute.String;
+    city: Schema.Attribute.String;
+    state: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    workSummery: Schema.Attribute.String;
+  };
+}
+
+export interface SkillsSkills extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
+  info: {
+    displayName: 'Skills';
+    icon: 'hashtag';
+  };
+  attributes: {
+    name: Schema.Attribute.String;
+    rating: Schema.Attribute.Integer;
+  };
+}
+
+export interface EducationEducation extends Struct.ComponentSchema {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'Education';
+    icon: 'code';
+  };
+  attributes: {
+    universityName: Schema.Attribute.String;
+    degree: Schema.Attribute.String;
+    major: Schema.Attribute.String;
+    startDate: Schema.Attribute.String;
+    endDate: Schema.Attribute.String;
+    description: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'experience.experience': ExperienceExperience;
+      'skills.skills': SkillsSkills;
+      'education.education': EducationEducation;
+    }
+  }
+}
